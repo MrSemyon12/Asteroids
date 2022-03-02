3d98955bd0f754102f301b9417b4389810f3aac7
MrSemyon12 <denisenko.box19@gmail.com>
2022-03-02 13:08:41 +1000

    starship sprites

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame, time
 from starship import Starship
 
+# screen settings
 pygame.init()
 WINDOW_SIZE = (980, 980)
 SCREEN = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Asteroids')
 last_time = time.time()
+
+# entities
 player = Starship()
 
 while True:
     dt = time.time() - last_time
+    dt *= 60
     last_time = time.time()
 
     SCREEN.fill((0, 0, 0))
@@ -17,7 +21,12 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit()        
+            quit()  
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+            player.runnig = 1
+        if event.type == pygame.KEYUP and event.key == pygame.K_w:
+            player.runnig = 0
+
 
     pygame.display.update() 
     
--- starship.py
+++ starship.py
@@ -2,9 +2,11 @@ import pygame
 
 
 class Starship:
-    def __init__(self):
-        self.pos = (400, 400)
-        self.image = pygame.image.load('data/starship.png') 
+    def __init__(self):        
+        self.image = [pygame.image.load('data/starship.png'), pygame.image.load('data/starship_running.png')]     
+        self.runnig = 0
+        self.pos = (470, 470)
+        self.vel = (0, 0)
 
     def draw(self, screen):
-        screen.blit(self.image, self.pos)
+        screen.blit(self.image[self.runnig], self.pos)
