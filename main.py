3a334c346c0a6e8cb854f3c531791093c7f9ed78
MrSemyon12 <denisenko.box19@gmail.com>
2022-03-02 19:19:31 +1000

    flying phisics

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import pygame, time
+import pygame, time, math
 from starship import Starship
+
+# constants
+WINDOW_SIZE = (980, 980)
+BLACK = (0, 0, 0)
 
 # screen settings
 pygame.init()
-WINDOW_SIZE = (980, 980)
 SCREEN = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Asteroids')
 last_time = time.time()
@@ -11,22 +14,36 @@ last_time = time.time()
 # entities
 player = Starship()
 
-while True:
+running = True
+while running:
     dt = time.time() - last_time
     dt *= 60
     last_time = time.time()
 
-    SCREEN.fill((0, 0, 0))
+    SCREEN.fill(BLACK) 
     player.draw(SCREEN)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()  
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False          
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             player.runnig = 1
         if event.type == pygame.KEYUP and event.key == pygame.K_w:
             player.runnig = 0
 
+    keys = pygame.key.get_pressed()
 
-    pygame.display.update() 
-    
+    if keys[pygame.K_w]:
+        player.dx += math.sin(math.radians(player.angle)) * -0.01 * dt
+        player.dy += math.cos(math.radians(player.angle)) * -0.01 * dt   
+    if keys[pygame.K_d]:
+        player.angle -= 0.25
+        player.angle %= 360
+    if keys[pygame.K_a]:
+        player.angle += 0.25
+        player.angle %= 360 
+
+    player.update()    
+    pygame.display.update()      
+
+pygame.quit()   
--- starship.py
+++ starship.py
@@ -5,8 +5,26 @@ class Starship:
     def __init__(self):        
         self.image = [pygame.image.load('data/starship.png'), pygame.image.load('data/starship_running.png')]     
         self.runnig = 0
-        self.pos = (470, 470)
-        self.vel = (0, 0)
+        self.x = 470
+        self.y = 470       
+        self.angle = 0
+        self.dx = 0
+        self.dy = 0
+
+    def update(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        if (self.x > 980):
+            self.x = 1
+        if (self.y > 980):
+            self.y = 1
+        if (self.x <= 0):
+            self.x = 980
+        if (self.y <= 0):
+            self.y = 980
 
     def draw(self, screen):
-        screen.blit(self.image[self.runnig], self.pos)
+        img_copy = pygame.transform.rotate(self.image[self.runnig], self.angle)
+        screen.blit(img_copy, (self.x - int(img_copy.get_width() / 2), self.y - int(img_copy.get_height() / 2)))   
+       
