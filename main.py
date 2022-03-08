04874f8e2119af7386307d813bb65c258a30a8da
MrSemyon12 <denisenko.box19@gmail.com>
2022-03-08 22:33:02 +1000

    menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, time, math
+import pygame, time, math, random
 from starship import Starship
 from asteroid import Asteroid
 from laser import Laser
@@ -7,21 +7,13 @@ from constants import *
 # Setup ----------------------------------------------------------- #
 pygame.init()
 pygame.display.set_caption('Asteroids')
+pygame.display.set_icon(pygame.image.load('data/asteroid_t1.png'))
 window_size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 flags = pygame.FULLSCREEN | pygame.DOUBLEBUF
 screen = pygame.display.set_mode(window_size, flags, 8)
 pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
 #window_size = (800, 800)
 #screen = pygame.display.set_mode(window_size, 0, 32)
-
-last_time = start_time = time.time()
-last_second = 0
-last_msecond = 0
-spawn_asteroid = 1
-spawn_laser = 1
-running = 1
-hard_lvl = 1
-lvl_up = 1
 
 # Images ---------------------------------------------------------- #
 asteroid_image = [pygame.image.load('data/asteroid_t1.png').convert_alpha(),
@@ -30,14 +22,66 @@ asteroid_image = [pygame.image.load('data/asteroid_t1.png').convert_alpha(),
 starship_image = [pygame.image.load('data/starship.png').convert_alpha(), 
                   pygame.image.load('data/starship_running.png').convert_alpha()]
 laser_image =     pygame.image.load('data/laser.png').convert_alpha()
+logo_image = pygame.transform.scale(pygame.image.load('data/logo.png').convert_alpha(), (1200, 400))
+playbutton_image = pygame.image.load('data/playbutton.png').convert_alpha()
 
-# Entities -------------------------------------------------------- #
+# Menu loop ------------------------------------------------------- #
+asteroids = []
+for i in range(10):
+    for j in range(3):
+        asteroids.append(Asteroid(1, window_size, asteroid_image, random.randint(100, window_size[0] - 100), random.randint(100, window_size[1] - 100)))
+    for j in range(4):
+        asteroids.append(Asteroid(2, window_size, asteroid_image, random.randint(100, window_size[0] - 100), random.randint(100, window_size[1] - 100)))
+    for j in range(3):
+        asteroids.append(Asteroid(3, window_size, asteroid_image, random.randint(100, window_size[0] - 100), random.randint(100, window_size[1] - 100)))
+    
+last_time = time.time()
+runningMenu = 1
+
+while runningMenu:
+    current_time = time.time()
+    dt = (current_time - last_time) * FPS
+    last_time = current_time
+    
+    mouseX, mouseY = pygame.mouse.get_pos()
+    clicked = 0
+
+    screen.fill(BLACK)
+    for ast in asteroids:
+        ast.draw(screen)
+    
+    screen.blit(logo_image, (window_size[0] // 2 - logo_image.get_width() // 2, window_size[1] // 2 - logo_image.get_height() // 2 + math.sin(current_time * 5) * 10 - 25))
+    screen.blit(playbutton_image, (window_size[0] // 2 - playbutton_image.get_width() // 2, window_size[1] // 2 - playbutton_image.get_height() // 2 + 100))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                clicked = 1
+    
+    for ast in asteroids:
+        ast.dx = ast.mx * dt
+        ast.dy = ast.my * dt
+        ast.dangle = ast.mangle * dt        
+        ast.update()
+
+    pygame.display.update()    
+
+# Start setup ----------------------------------------------------- #
 starship = Starship(window_size, starship_image)
 asteroids = []
 lasers = []
+last_time = start_time = time.time()
+last_second = 0
+last_msecond = 0
+spawn_asteroid = 1
+spawn_laser = 1
+hard_lvl = 1
+lvl_up = 1
+runningGame = 1
 
 # Main loop ------------------------------------------------------- #
-while running:      
+while runningGame:      
     current_time = time.time()
     dt = (current_time - last_time) * FPS   
     second = last_time - start_time + 1
@@ -78,12 +122,12 @@ while running:
         ast.draw(screen)
     for las in lasers:
         las.draw(screen)
-    starship.draw(screen)    
-
+    starship.draw(screen)  
+    
     # Buttons ----------------------------------------------------- #
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            running = 0          
+            runningGame = 0          
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
             starship.runnig = 1
         if event.type == pygame.KEYUP and event.key == pygame.K_w:
